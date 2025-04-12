1efcb7e6b4c951237302776e1e14fd770ae3439b
Ostapchuk-Roman <roma.ostapchuk2016@mail.com>
2025-04-12 11:37:51 +0300

    fixed speaker-section

--- js/tooltip-triger_sctipt.js
+++ js/tooltip-triger_sctipt.js
@@ -1,8 +1,24 @@
-const tooltipTrigger = document.querySelector(".tooltip-trigger");
-const tooltipText = document.querySelector(".tooltip-text");
+const tooltipTriggers = document.querySelectorAll(".tooltip-trigger");
+const tooltipTexts = document.querySelectorAll(".tooltip-text");
 
-tooltipTrigger.addEventListener("click", function () {
-  tooltipText.style.visibility =
-    tooltipText.style.visibility === "visible" ? "hidden" : "visible";
-  tooltipText.style.opacity = tooltipText.style.opacity === "1" ? "0" : "1";
+tooltipTriggers.forEach(function (tooltipTrigger, index) {
+  const tooltipText = tooltipTexts[index];
+
+  tooltipTrigger.addEventListener("click", function (event) {
+    // Змінюємо видимість підказки для натиснутого елемента
+    tooltipText.style.visibility =
+      tooltipText.style.visibility === "visible" ? "hidden" : "visible";
+    tooltipText.style.opacity = tooltipText.style.opacity === "1" ? "0" : "1";
+
+    // Запобігаємо поширенню події, щоб не спрацював обробник для document
+    event.stopPropagation();
+  });
+});
+
+// Додаємо обробник події для кліку в будь-яке місце на сторінці
+document.addEventListener("click", function () {
+  tooltipTexts.forEach(function (tooltipText) {
+    tooltipText.style.visibility = "hidden";
+    tooltipText.style.opacity = "0";
+  });
 });
